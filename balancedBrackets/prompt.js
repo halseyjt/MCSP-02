b08e10d5c245a51cbe759f6cae01997035facdd9
halseyjt <halseyjt@yahoo.com>
2019-12-17 10:01:31 -0600

    balancedBrackets attempted but did not get a result

--- balancedBrackets/prompt.js
+++ balancedBrackets/prompt.js
@@ -9,4 +9,36 @@
 // str: "[{()}]"                  |   true
 // str: "(50)("                   |   false
 // str: "[{]}"                    |   false
-const isBalanced = str => {};
+const isBalanced = (str) => {
+    //  set a stack variable to an empty array
+    let stack = [];
+    // set a map variable to an object with the key value pairs being opening and closing brackets
+    let map = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
+    // next, iterate through the given string.
+    for (let i = 0; i < str.length; i++) {
+        // compare each character to see if they are an opening bracket
+        if (str[i] === '[' || str[i] === '{' || str[i] === '[') {
+            // if the character is an opening bracket push that into the stack
+            stack.push(str[i]);
+            // else if the character is a closing bracket remove them from the string
+        } else {
+            let last = stack.pop()
+            // if the last element removed doesn't match the opening bracket, return false
+            if (str[i] !== map[last]) {
+                return false;
+            }
+        }
+        // if the length of the stack does not equal zero, return false
+        if (stack.length !== 0) {
+            return false;
+        }
+        return true;
+    }
+};    
+
+const test = isBalanced("(x+y) - (4)");
+console.log(test)
