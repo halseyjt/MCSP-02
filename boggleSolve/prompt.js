0819d1789c23acec96b58b2d007e64493f4f0d16
halseyjt <halseyjt@yahoo.com>
2020-01-14 09:56:21 -0600

    boggleSolve attempted

--- boggleSolve/prompt.js
+++ boggleSolve/prompt.js
@@ -3,4 +3,20 @@
 // * i.e. a=1, z=26 etc.
 // return the sum of the values of each letter in string
 
-const boggleSolve = str => {};
+const boggleSolve = str => { };
+let str = 'abcdefghijklmnopqrstuvwxyz'
+// set an array to the alphabet
+let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+// create an empty array
+// iterate through the letters array
+for (let i = 0; i < letters.length; i++){
+    // iterate through the string
+    for (let j = 0; j < str.length; i++) {
+    // compare the string elements to the letters elements
+        if (str[j] === letters[i]) {
+            str[j] = letters.indexOf(i);
+        }
+    }
+    return str[j]  + 1
+}
+
