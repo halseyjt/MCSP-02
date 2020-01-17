c9cdaf41bf821766914f0e65d868b5ed5e655918
halseyjt <halseyjt@yahoo.com>
2020-01-17 09:44:22 -0600

    longestPalindrome completed

--- boggleSolve/prompt.js
+++ boggleSolve/prompt.js
@@ -3,20 +3,28 @@
 // * i.e. a=1, z=26 etc.
 // return the sum of the values of each letter in string
 
-const boggleSolve = str => { };
-let str = 'abcdefghijklmnopqrstuvwxyz'
-// set an array to the alphabet
-let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-// create an empty array
-// iterate through the letters array
-for (let i = 0; i < letters.length; i++){
-    // iterate through the string
-    for (let j = 0; j < str.length; i++) {
-    // compare the string elements to the letters elements
-        if (str[j] === letters[i]) {
-            str[j] = letters.indexOf(i);
-        }
-    }
-    return str[j]  + 1
-}
+// const boggleSolve = str => { };
+// let str = 'abcdefghijklmnopqrstuvwxyz'
+// // set an array to the alphabet
+// let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+// // create an empty array
+// // iterate through the letters array
+// for (let i = 0; i < letters.length; i++){
+//     // iterate through the string
+//     for (let j = 0; j < str.length; i++) {
+//     // compare the string elements to the letters elements
+//         if (str[j] === letters[i]) {
+//             str[j] = letters.indexOf(i);
+//         }
+//     }
+//     return str[j]  + 1
+// }
+
+
+// const boggleSolve = str => { };
+// var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+// var letters = str.split('');
+//     return letters.reduce((sum, i) => {
+//         return alphabet.indexOf(letters[i])
+// });
 
--- longestPalindrome/prompt.js
+++ longestPalindrome/prompt.js
@@ -6,4 +6,28 @@
  * whitespace on each side of dad).
  */
 
-var longestPalindrome = function(string) {};
+var isPalindrome = function (string) {
+    var rev = string.split("").reverse().join("");
+    return string == rev;   
+};
+var longestPalindrome = function (string) {
+    var maxLength = 0;
+    var maxP = '';
+    for (var i = 0; i < string.length; i++){
+        var subs = string.substr(i, string.length);
+        for (var j = subs.length; j >= 0; j--){
+            var subSubsStr = subs.substr(0, j);
+            if (subSubsStr.length <= 1) {
+                continue;
+            }    
+                if (isPalindrome(subSubsStr)) {
+                    if (subSubsStr.length > maxLength) {
+                        maxLength = subSubsStr.length;
+                        maxP = subSubsStr
+                }
+            }
+        }    
+    }
+    return maxP;
+ };
+console.log(longestPalindrome("My dad is a racecar athlete"));
