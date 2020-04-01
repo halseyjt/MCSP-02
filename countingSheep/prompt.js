f4ed6c7c619161adc9eca047be04fd39fbc1fe35
Joshua Halsey <joshuahalsey@Joshuas-MacBook-Pro.local>
2020-04-01 12:28:29 -0500

    counting sheep completed

--- countingSheep/prompt.js
+++ countingSheep/prompt.js
@@ -9,3 +9,31 @@
 //         true, true, true, true,
 //         false, false, true, true]
 // The correct answer would be 17.
+
+// I: array consisting of sheep present and sheep missing  
+// O: number of sheep present
+// C: 
+// E: 
+
+
+// iterate through the array
+// compare each array element to see if the element is present or missing
+// 
+
+   var arrOfSheep = [
+     true, true, true,
+     false, true, true,
+     true, true, true,
+     false, true, false,
+     true, false, false,
+     true, true, true,
+     true, true, false,
+     false, true, true
+   ];
+
+   function countSheep(arrOfSheep) {
+     return arrOfSheep.filter(x => x === true).length;
+   }
+
+   console.log(countSheep(arrOfSheep));
+
